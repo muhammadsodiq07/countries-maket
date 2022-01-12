461ee5014379c7577d0ec82843388f29a12f394c
muhammadsodiq07 <muhammadsodiqkoziyev1@gmail.com>
2022-01-12 21:06:18 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let a = 0;
+document.getElementById('modeBtn') .addEventListener('click' ,function () {
+    document.body.classList.toggle('dark');
+    if (a == 0) {
+        this.textContent = 'Light mode';
+        a++;
+    } else {
+        this.textContent = 'Dark mode';
+        a = 0;
+    }
+
+})
